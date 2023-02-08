b9842781779747f55ee6f19815616bb933e15a6a
Emmanuel Obeng Twene <delinuxist@gmail.com>
2023-02-08 14:53:08 +0000

    bookmark service and controller implemented

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateBookMarkDto, EditBookmarkDto } from './dto';
 
 @Injectable({})
-export class BookmarkService {}
+export class BookmarkService {
+  constructor(private prisma: PrismaService) {}
+
+  getBookmarks(userId: number) {
+    return this.prisma.bookmark.findMany({
+      where: {
+        userId,
+      },
+    });
+  }
+
+  async getBookmarkById(userId: number, bookmarkId: number) {
+    const bookmark = await this.prisma.bookmark.findFirst({
+      where: {
+        userId: userId,
+        id: bookmarkId,
+      },
+    });
+    return bookmark;
+  }
+
+  async createBookmark(userId: number, dto: CreateBookMarkDto) {
+    const bookmark = await this.prisma.bookmark.create({
+      data: {
+        ...dto,
+        userId,
+      },
+    });
+
+    return bookmark;
+  }
+
+  async editBookmarkById(
+    userId: number,
+    bookmarkId: number,
+    dto: EditBookmarkDto,
+  ) {
+    const bookmark = await this.prisma.bookmark.findFirst({
+      where: {
+        id: bookmarkId,
+        userId: userId,
+      },
+    });
+
+    if (!bookmark) throw new ForbiddenException('Access to resources denied');
+
+    return this.prisma.bookmark.update({
+      where: {
+        id: bookmarkId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+  }
+
+  async deleteBookmarkById(userId: number, bookmarkId: number) {
+    const bookmark = await this.prisma.bookmark.findFirst({
+      where: {
+        id: bookmarkId,
+        userId: userId,
+      },
+    });
+
+    if (!bookmark) throw new ForbiddenException('Access to resources denied');
+
+    return this.prisma.bookmark.delete({
+      where: {
+        id: bookmarkId,
+      },
+    });
+  }
+}
